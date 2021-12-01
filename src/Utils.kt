58be17d5245f6e2ea525304fa35ec5b520c14d62
Sam Cooper <66436650+samcooper720x@users.noreply.github.com>
2021-12-01 13:09:13 +0000

    Day01: Gold star

--- src/Utils.kt
+++ src/Utils.kt
@@ -5,7 +5,8 @@ import java.security.MessageDigest
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt").readLines()
+fun readInputAsStrs(name: String) = File("src", "$name.txt").readLines()
+fun readInputAsInts(name: String) = File("src", "$name.txt").readLines().map { it.toInt() }
 
 /**
  * Converts string to md5 hash.
