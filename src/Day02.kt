d821ad380013577f7d2c2545de32b68cb85033fd
Sam Cooper <66436650+samcooper720x@users.noreply.github.com>
2021-12-02 18:47:16 +0000

    refactor: Use fold

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,47 +1,34 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        var x = 0
-        var y = 0
-        input.map {
-            val command = it.split(" ")
-            val direction = command.first()
-            val distance = command.last().toInt()
+    fun part1(input: List<String>): Int = input
+        .fold(mutableMapOf("x" to 0, "y" to 0)) { position, command ->
+            val direction = command.split(" ").first()
+            val distance = command.split(" ").last().toInt()
             when (direction) {
-                "forward" -> x += distance
-                "down" -> y += distance
-                "up" -> y -= distance
-                else -> println("incompatible direction received")
+                "forward" -> position.merge("x", distance, Int::plus)
+                "down" -> position.merge("y", distance, Int::plus)
+                "up" -> position.merge("y", distance, Int::minus)
             }
-        }
-        return x * y
-    }
+            position
+        }.values.reduce { product, coordinate -> product * coordinate }
 
-    fun part2(input: List<String>): Int {
-        var aim = 0
-        var x = 0
-        var y = 0
-        input.map {
-            val command = it.split(" ")
-            val direction = command.first()
-            val distance = command.last().toInt()
+    fun part2(input: List<String>): Int = input
+        .fold(mutableMapOf("aim" to 0, "x" to 0, "y" to 0)) { position, command ->
+            val direction = command.split(" ").first()
+            val distance = command.split(" ").last().toInt()
             when (direction) {
                 "forward" -> {
-                    x += distance
-                    y += aim * distance
+                    position.merge("x", distance, Int::plus)
+                    position.merge("y", position.getValue("aim") * distance, Int::plus)
                 }
-                "down" -> aim += distance
-                "up" -> aim -= distance
-                else -> println("incompatible direction received")
+                "down" -> position.merge("aim", distance, Int::plus)
+                "up" -> position.merge("aim", distance, Int::minus)
             }
-        }
-        return x * y
-    }
+            position
+        }.values.drop(1).reduce { product, coordinate -> product * coordinate }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInputAsStrs("Day02_test")
-    println(part1(testInput))
     check(part1(testInput) == 150)
-    println(part2(testInput))
     check(part2(testInput) == 900)
 
     val input = readInputAsStrs("Day02")
